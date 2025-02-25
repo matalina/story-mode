b13b82a1419498af65957fd865a05bc388fe07b3
Alicia Wilkerson <matalina@gmail.com>
2025-02-25 16:09:20 -0600

    removed last map peice

--- src/components/pages/Map.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import CartaMap from "../carta-map/CartaMap.svelte";
-  import Page from "../ui/Page.svelte";
-</script>
-
-<Page back="/story">
-  <h3 class="text-3xl font-bold mb-3">Map</h3>
-  <CartaMap />
-</Page>
