baf8db07975ae98605316acab31aa46f3438ddc2
Alicia Wilkerson <matalina@gmail.com>
2025-02-25 16:02:06 -0600

    removed map

--- src/components/ui/Header.svelte
+++ src/components/ui/Header.svelte
@@ -3,8 +3,6 @@
   import Logo from "../../assets/logo.svg";
   import ClearButton from "../story-mode/ClearButton.svelte";
   import CopyButton from "../story-mode/CopyButton.svelte";
-  import Divider from "./Divider.svelte";
-  import MapButton from "../carta-map/MapButton.svelte";
 </script>
 
 <header class="flex justify-between items-center mb-3">
@@ -12,8 +10,6 @@
     <img src={Logo} alt="Story Mode" class="h-10" />
   </Link>
   <div class="flex gap-2 items-center">
-    <MapButton />
-    <Divider />
     <ClearButton/>
     <CopyButton />
   </div>
